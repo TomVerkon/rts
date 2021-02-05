6e994d32cb0f842fc549b8a6869a24834e19dcd9
Tom Verkon <tverkon@usa.net>
2021-02-05 09:47:02 -0500

    End of props demo, state demo next

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,23 +1,33 @@
+import React, { FC } from 'react';
 interface ChildProps {
   color: string;
+  onClick: () => void;
 }
 
 // this will work but typescript doesn't understand this is a React Component definition
-export const Child = ({ color }: ChildProps) => {
+// Also to see the children prop, it has to be defined in interface
+export const Child = ({ color, onClick }: ChildProps) => {
   return (
     <div>
       <h1>{color}</h1>
+      <button type="button" onClick={onClick}>
+        click me
+      </button>
     </div>
   );
 };
 
 Child.displayName = 'Child';
 
-// This is seen as a React Component by typescript.
-export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+// This is seen as a React Component by typescript. It supports children without defininh in interface
+export const ChildAsFC: FC<ChildProps> = ({ color, onClick, children }) => {
   return (
     <div>
       <h1>{color}</h1>
+      {children}{' '}
+      <button type="button" onClick={onClick}>
+        Click me
+      </button>
     </div>
   );
 };
